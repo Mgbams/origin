a79cf9a3549b6a066a810e7ca36129820b09d106
kingsley <cmgbams@gmail.com>
2020-04-03 08:13:06 +0200

    modified contact page

--- src/app/contact-info/contact-info.component.ts
+++ src/app/contact-info/contact-info.component.ts
@@ -10,6 +10,7 @@ export class ContactInfoComponent implements OnInit {
   public contactInfoForm;
 
   constructor(private formBuilder: FormBuilder) { 
+    // Remember to add validations later
     this.contactInfoForm = this.formBuilder.group({
       country: [''],
       state: [''],
