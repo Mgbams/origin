e3b5616727ba934d65472fb9fd277137c618eede
kingsley <cmgbams@gmail.com>
2020-05-21 07:33:46 +0200

    checkout page with paypal

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,22 +1,38 @@
 import { CartModalComponent } from './cart-modal/cart-modal.component';
 import { CartService } from './../shared/services/cart.service';
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { ToastController, ModalController } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
 import { AllProducts } from './../shared/models/allProducts';
+
+declare let paypal: any;
+/* Fake credit card generator website: http://ccardgenerator.com/generat-visa-card-numbers.php
+* My default credit card details:
+* Issuing network: Visa
+* Card number: 4374487489797489
+* prenom: Rihanna 
+* Nom: Baker
+* Adress: 5 rue helene boucher
+* Country: france
+* city: lyon 
+* CVV: 775
+* Exp: 04/23
+* postal code: 69008
+* telephone +33 09 87 76 66 68
+* email: g@gmail.com
+*/
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, AfterViewInit {
   public cart: AllProducts[] = [];
   public products;
   public cartItemCount: BehaviorSubject<number>;
- 
-   // @ViewChild('cart', {static: false, read: ElementRef}) fab: ElementRef;
- 
+  public paidFor = false;
+
   constructor(
     private cartService: CartService,
     private modalCtrl: ModalController,
@@ -31,36 +47,59 @@ export class CartComponent implements OnInit {
     // this.animateCSS('tada');
   }
 
- /*  addToCart(product) {
-    this.cartService.addProduct(product);
-    this.animateCSS('tada');
-  } */
-
-  async openCart() {
-   // this.animateCSS('bounceOutLeft', true);
-
-    const modal = await this.modalCtrl.create({
-      component: CartModalComponent,
-      cssClass: 'cart-modal'
+  ngAfterViewInit(): void {
+    this.loadExternalScript('https://www.paypalobjects.com/api/checkout.js').then(() => {
+      paypal.Button.render({
+        env: 'sandbox',
+        client: {
+          production: '',
+          sandbox: 'AY2WuPpZbWYBCoBoDqgppp7NlDl1QBj6-SRytmTGrCNaLa_X0A0BQS-poxq0-tkYww0hqUfH2fkQWftP'
+        },
+        commit: true,
+        // customize button (optional)
+        style: {
+          size: 'small',
+          color: 'gold',
+          shape: 'pill',
+        },
+        locale: 'fr_FR',
+        payment: (data, actions) => {
+          return actions.payment.create({
+            payment: {
+              transactions: [
+                {
+                  amount: { total: this.getTotal(), currency: 'EUR' }
+                }
+              ]
+            }
+          });
+        },
+        onAuthorize: (data, actions) => {
+          return actions.payment.execute().then((payment) => {
+            // TODO
+            window.alert('Thank you for your purchase');
+            this.paidFor = true;
+            this.cartService.clearCart(); // Clear carte after payment
+            this.cartService.getCart();
+            this.getTotal(); // clears the total as there would be nothing in it after clearing the carte above
+            this.cartItemCount.next(this.cartItemCount.value * 0); // clear item counter
+          });
+        },
+        onError: err => {
+          console.log(err);
+        }
+      }, '#paypal');
     });
-   /*  modal.onWillDismiss().then(() => {
-      this.fab.nativeElement.classList.remove('animated', 'bounceOutLeft');
-      this.animateCSS('bounceInLeft');
-    }); */
-    modal.present();
   }
- 
- /* animateCSS(animationName, keepAnimated = false) {
-    const node = this.fab.nativeElement;
-    node.classList.add('animated', animationName);
-    function handleAnimationEnd() {
-      if (!keepAnimated) {
-        node.classList.remove('animated', animationName);
-      }
-      node.removeEventListener('animationend', handleAnimationEnd);
-    }
-    node.addEventListener('animationend', handleAnimationEnd);
-  }*/
+
+  private loadExternalScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = scriptUrl;
+      scriptElement.onload = resolve;
+      document.body.appendChild(scriptElement);
+  });
+  }
 
   getTotal() {
     return this.cart.reduce((i, j) => i + j[0].product_price * j.units_on_order, 0);
