a010e50e571feddb674a7c3afebab31b4f7694cc
kingsley <cmgbams@gmail.com>
2020-05-29 09:52:45 +0200

    tab3 page updates

--- src/app/my-account/my-account-routing.module.ts
+++ src/app/my-account/my-account-routing.module.ts
@@ -1,7 +1,5 @@
-import { MyAccountComponent } from './my-account.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
 
 
 @NgModule({
